a326f0ff49bee253b60b7f6177b70cda761b1246
Filipe Monteiro <pi.monteiro1998@gmail.com>
2017-09-30 00:20:14 +0100

    okay

--- rythm.py
+++ rythm.py
@@ -1,25 +1,24 @@
 from miditime.miditime import MIDITime
-import random
 from datetime import datetime
 from scapy.all import *
 import argparse
 
 random.seed(datetime.now())
 
-#Protocols Position on Music
+# Protocols Position on Music
 p_dhcp = 1
 p_tcp = 2
-p_udp = 3 
+p_udp = 3
 p_arp = 4
 
-#Compare scales
+# Compare scales
 c = [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
 csh = [13, 25, 37, 49, 61, 73, 85, 97, 109, 121]
 d = [14, 26, 38, 50, 62, 74, 86, 98, 110, 122]
 dsh = [15, 27, 39, 51, 63, 75, 87, 99, 111, 123]
-e = [16, 28, 40, 52, 64, 76, 88, 100, 112, 124] 
+e = [16, 28, 40, 52, 64, 76, 88, 100, 112, 124]
 f = [17, 29, 41, 53, 65, 77, 89, 101, 113, 125]
-fsh = [18, 30, 42, 54, 66, 78, 90, 102, 114, 126] 
+fsh = [18, 30, 42, 54, 66, 78, 90, 102, 114, 126]
 g = [19, 31, 43, 55, 67, 79, 91, 103, 115, 127]
 gsh = [20, 32, 44, 56, 68, 80, 92, 104, 116]
 a = [21, 33, 45, 57, 69, 81, 93, 105, 117]
@@ -30,30 +29,29 @@ do = [c, d, e, f, g, a, b]
 dosh = [csh, dsh, f, fsh, gsh, ash, c]
 re = [d, e, fsh, g, a, b, csh]
 resh = [dsh, f, g, gsh, ash, c, d]
-mi = [e, fsh, gsh, a, b, csh, dsh] 
+mi = [e, fsh, gsh, a, b, csh, dsh]
 fa = [f, g, a, ash, c, d, e]
-fash = [fsh, gsh, ash, b, csh, dsh, f] 
+fash = [fsh, gsh, ash, b, csh, dsh, f]
 sol = [g, a, b, c, d, e, fsh]
 solsh = [gsh, ash, c, csh, dsh, f, g]
 la = [a, b, csh, d, e, fsh, gsh]
 lash = [ash, c, d, dsh, f, g, a]
 si = [b, csh, dsh, e, fsh, gsh, ash]
 
-escalas = [ do, dosh, re, mi, fa, fash, sol, solsh, la, lash, si]
+escalas = [do, dosh, re, mi, fa, fash, sol, solsh, la, lash, si]
 
-#Array with notes to be played
+# Array with notes to be played
 midinotes = []
 notes_tcp = []
 notes_udp = []
 notes_arp = []
 notes_dhcp = []
 
-
-#arguementos
+# arguementos
 ap = argparse.ArgumentParser()
 ap.add_argument('-f', '--file', required=True, help='.pcap file to filter')
 
-#tratar de argumentos
+# tratar de argumentos
 args = vars(ap.parse_args())
 ficheiro = args["file"]
 
@@ -62,144 +60,147 @@ udp = []
 arp = []
 dhcp = []
 
-#carrega cap
+# carrega cap
 pacotes = rdpcap(ficheiro)
 for pacote in pacotes:
-	#tem conteudo
-	if(pacote.haslayer(Raw)):
-		#conteudo
-		test = pacote[Raw].load
-		#conteudo em int
-		test_int = int.from_bytes(test, byteorder='little')
-		#conteudo binario
-		load = bin(test_int)
-		if(pacote.haslayer(UDP)):
-			udp.append(load)
-		if(pacote.haslayer(TCP)):
-			tcp.append(load)
-	if(pacote.haslayer(Padding)):
-			test = pacote[Padding].load
-			test_int = int.from_bytes(test, byteorder='little')
-			load=bin(test_int)
-			if(pacote.haslayer(ARP)):
-				arp.append(load)
-			if(pacote.haslayer(DHCP)):
-				dhcp.append(load)
+    # tem conteudo
+    if pacote.haslayer(Raw):
+        # conteudo
+        test = pacote[Raw].load
+        # conteudo em int
+        test_int = int.from_bytes(test, byteorder='little')
+        # conteudo binario
+        load = bin(test_int)
+        if pacote.haslayer(UDP):
+            udp.append(load)
+        if pacote.haslayer(TCP):
+            tcp.append(load)
+    if pacote.haslayer(Padding):
+        test = pacote[Padding].load
+        test_int = int.from_bytes(test, byteorder='little')
+        load = bin(test_int)
+        if pacote.haslayer(ARP):
+            arp.append(load)
+        if pacote.haslayer(DHCP):
+            dhcp.append(load)
 
 
 def clean_list(lista):
-	for i in range(len(lista)):
-		codigo = lista[i][2:] 
-		lista[i] = codigo
+    for i in range(len(lista)):
+        codigo = lista[i][2:]
+        lista[i] = codigo
+
 
 def clean_listas():
-	clean_list(udp)
-	clean_list(tcp)
-	clean_list(arp)
-	clean_list(dhcp)
+    clean_list(udp)
+    clean_list(tcp)
+    clean_list(arp)
+    clean_list(dhcp)
 
 
 def pertence_a_escala(x):
-	for nota in escalas[0]:
-		if(x in nota):
-			return 1
-	return 0
+    for nota in escalas[0]:
+        if x in nota:
+            return 1
+    return 0
+
+
 # Conversor
 def strcode(code, arrai, position):
-	escala = 0
-	flag = 0
-	if (code==""):
-		if position == 1:
-			arrai.append(random.randint(12, 35))	
-			#print("ERRO" + "codigo: " + code)
-		if position == 3:
-			arrai.append(random.randint(12, 127))
-		if position == 2:
-			arrai.append(random.randint(60, 84))
-		else:
-			print("ERRO " + code)
-		return
-	tamanho = int(len(code)/100)
-	while (tamanho>0):
-		note = code[:7]
-		code = code[8:]
-		x = note_conv(note)
-		for nota in escalas[0]:
-			if(x in nota):
-				flag += 1
-		# se tiver na escala
-		break_l=0;
-		if(flag != 0):
-			while True:
-				if((position == 1 and (x < 12 or x > 35)) or not pertence_a_escala(x)):
-					x = random.randint(12, 35) #notas de um baixo, segundo o senpai
-				if((position == 2 and (x < 60 or x > 84)) or (position == 4 and (x < 100 or x > 127))):
-					tamanho -= 7
-					break
-				
-				if(pertence_a_escala(x)):
-					arrai.append(x)
-					tamanho -= 7
-					break
+    escala = 0
+    flag = 0
+    if code == "":
+        if position == 1:
+            arrai.append(random.randint(12, 35))
+        # print("ERRO" + "codigo: " + code)
+        if position == 3:
+            arrai.append(random.randint(12, 127))
+        if position == 2:
+            arrai.append(random.randint(60, 84))
+        else:
+            print("ERRO " + code)
+        return
+    tamanho = int(len(code) / 10)
+    while tamanho > 0:
+        #print("FODA-SE")
+        y = 7
+        note = code[:y]
+        code = code[(y+1):]
+        x = note_conv(note, y)
+        for nota in escalas[0]:
+            if x in nota:
+                flag += 1
+        # se tiver na escala
+        if flag != 0:
+            while True:
+                print("Nao saio daqui")
+                if (position == 1 and (x < 12 or x > 35)) or not pertence_a_escala(x):
+                    x = random.randint(12, 35)  # notas de um baixo, segundo o senpai
+                if (position == 2 and (x < 60 or x > 84)) or (position == 4 and (x < 100 or x > 127)):
+                    tamanho -= y
+                    break
+
+                if pertence_a_escala(x):
+                    arrai.append(x)
+                    tamanho -= y
+                    break
 
 
-
-def note_conv(note):
-	e = 0
-	x = 0
-	if(len(note)==7):
-		for i in range(0, 6):
-		#x = 2**e + x
-		#e += 1
-			if(int(note[i])==1):
-				x = 2**i + x
-	return x
+def note_conv(note, y):
+    x = 0
+    if len(note) == y:
+        for i in range(0, y-1):
+            if int(note[i]) == 1:
+                x = 2 ** i + x
+    return x
 
 
 def make_notes():
-	strcode(f_tcp, notes_tcp, p_tcp)
-	set_note_array(notes_tcp, 1)
-
-	strcode(f_udp, notes_udp, p_udp)
-	set_note_array(notes_udp, 1)
-	
-	strcode(f_arp, notes_arp, p_arp)
-	set_note_array(notes_arp, 2)
-	
-	strcode(f_dhcp, notes_dhcp, p_dhcp)
-	set_note_array(notes_dhcp, 3) #Bass
-
+    strcode(f_tcp, notes_tcp, p_tcp)
+    set_note_array(notes_tcp, 1)
+    print("TCP DONE")
+    print(notes_tcp)
+    strcode(f_udp, notes_udp, p_udp)
+    set_note_array(notes_udp, 1)
+    print("UDP DONE")
+    strcode(f_arp, notes_arp, p_arp)
+    set_note_array(notes_arp, 2)
+    print("ARP DONE")
+    strcode(f_dhcp, notes_dhcp, p_dhcp)
+    set_note_array(notes_dhcp, 3)  # Bass
+    print("DHCP DONE")
 
 def set_note_array(arrai, PROTOCOL):
-	j = 0
-	#loop to go through all the available notes
-	for i in arrai:
-		rnd  = random.randint(0,2)
-		midinotes.append([j + rnd, i, 127, PROTOCOL])
-		j = j + 1 + rnd
+    j = 0
+    # loop to go through all the available notes
+    for i in arrai:
+        rnd = random.randint(0, 2)
+        midinotes.append([j + rnd, i, 127, PROTOCOL])
+        j = j + 1 + rnd
 
-#Rythm of the music
+
+# Rythm of the music
 BPM = 250
 
-#Inicialize song
+# Inicialize song
 song = MIDITime(BPM, 'teste.mid')
 song.add_track(midinotes)
 
-#main
-#Output of the MIDI data to a file.mid
+# main
+# Output of the MIDI data to a file.mid
 clean_listas()
 f_udp = f_arp = f_dhcp = f_tcp = ""
 for i in range(len(udp)):
-	f_udp += udp[i]
+    f_udp += udp[i]
 for i in range(len(tcp)):
-	f_tcp += tcp[i]
+    f_tcp += tcp[i]
 for i in range(len(dhcp)):
-	f_dhcp += dhcp[i]
+    f_dhcp += dhcp[i]
 for i in range(len(arp)):
-	f_arp += arp[i]
+    f_arp += arp[i]
 
-#print(len(f_udp)+len(f_tcp)+len(f_arp)+len(f_dhcp))
+# print(len(f_udp)+len(f_tcp)+len(f_arp)+len(f_dhcp))
 make_notes()
-#print("NOTAS DHCP: \n")
-#print(*notes_dhcp, sep='\n')
+# print("NOTAS DHCP: \n")
+# print(*notes_dhcp, sep='\n')
 song.save_midi()
